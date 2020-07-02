4ae347d5b0d47b5ce352f4b3abdaf5e5bf5729b6
Martynov Dmitry <info@mgmediacom.ru>
2020-07-02 14:50:01 +0300

    Решение задачи 2-1

--- 2-module/1-task/CounterButton.js
+++ 2-module/1-task/CounterButton.js
@@ -1,10 +1,30 @@
+const template = '<button type="button" @click="clickHandler">{{clickCount}}</button>';
+
 export const CounterButton = {
-  // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template,
 
-  // Компонент должен иметь пропс
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    }
+  },
 
-  // Компонент должен иметь модель
+  data() {
+    return {
+      clickCount: this.count === undefined || this.count === null ? 0 : this.count
+    }
+  },
 
-  // Шаблон лучше иметь максимально простым, а логику выносить в методы
+  model: {
+    prop: 'count',
+    event: 'increment'
+  },
+
+  methods: {
+    clickHandler(e) {
+      this.clickCount++;
+      this.$emit('increment', this.clickCount);
+    }
+  }
 };
