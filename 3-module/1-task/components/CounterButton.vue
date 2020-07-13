ffc0e3c31b4537bff2e40dac239549058a76687a
d.s.martynov <d.s.martynov@diacare-soft.ru>
2020-07-13 20:52:33 +0300

    Решение задачи 3-1

---
+++ 3-module/1-task/components/CounterButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button @click="handleClick">{{ localCount }}</button>
+</template>
+
+<script>
+  export default {
+    props: {
+      count: {
+        type: Number,
+        default: 0
+      }
+    },
+    model: {
+      prop: 'count',
+      event: 'increment'
+    },
+    data() {
+      return {
+        localCount: this.count === undefined || this.count == null ? 0 : this.count
+      }
+    },
+    methods: {
+      handleClick() {
+        this.localCount++;
+        this.$emit('increment', this.localCount);
+      }
+    },
+    watch: {
+      'count'(value) {
+        this.localCount = value;
+      }
+    }
+  };
+</script>
+
+<style></style>
